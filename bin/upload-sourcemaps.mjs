38e9e3a334d1c5f4dd5635f3090e2c47bc18443e
EJ Mason <13525251+mxmason@users.noreply.github.com>
2025-04-08 16:56:37 -0700

    chore: add explainer comments to upload script

--- bin/upload-sourcemaps.mjs
+++ bin/upload-sourcemaps.mjs
@@ -4,19 +4,25 @@ import 'zx/globals';
 
 $.verbose = true;
 
+// datadog-ci needs these env variables to be set.
+// We have them namespaced under VITE_ in .env and in Netlify config,
+// but that's not good enough
 process.env.DATADOG_API_KEY = process.env.VITE_DATADOG_API_KEY;
 process.env.DATADOG_SITE = process.env.VITE_DATADOG_SITE;
 
 const gitRev = await $`git rev-parse HEAD`;
 
-const ddCIFlags = [
+const uploadArgs = [
+	// The location of the runtime JS assets
+	'./dist/assets',
+	// The path to the source maps
 	`--minified-path-prefix=/assets/`,
 	`--release-version=${gitRev}`,
 	'--service=test',
 ];
 
 try {
-	await $`datadog-ci sourcemaps upload ./dist/assets ${ddCIFlags}`;
+	await $`datadog-ci sourcemaps upload ${uploadArgs}`;
 } catch (error) {
 	console.error('Error uploading sourcemaps:', error);
 	process.exit(1);
